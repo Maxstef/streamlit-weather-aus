0db1302867b8c388a0530d6f3a979569883c115c
Maks Stefanko <casstefaks@gmail.com>
2025-10-03 13:01:03 +0300

    init streamlit app

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+# titles and intro
+st.set_page_config(page_title="Weather in Australia Tomorrow", page_icon="🌦️")
+st.title('Weather in Australia Tomorrow')
+st.markdown('That is simple model to predict the rain in Australia tomorrow')
+st.image('images/weather-aus.png')
